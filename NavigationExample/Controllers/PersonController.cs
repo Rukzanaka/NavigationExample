a48ddec6da54588e996fbcd2bcc63675410f6b34
Rukzanaka <sub9zan1@gmail.com>
2021-03-29 07:47:49 +0530

    Add project files.

---
+++ NavigationExample/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NavigationExample.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult PersonList()
+        {
+            return View();
+        }
+        public ActionResult PersonAdd()
+        {
+            return View();
+        }
+        public ActionResult PersonDelete()
+        {
+            return View();
+        }
+
+    }
+}
